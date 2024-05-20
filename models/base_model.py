d1fd7f9ab63430b924c33b80e7c90543d7509064
Bigsam08 <uelsam994@gmail.com>
2024-05-19 18:32:37 -0700

    ok

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ for initialization, serilization and deserialization of file storage
 lets enjoy the fun
 """
 
-from uuid import uuid4
+import uuid
 import models
 from datetime import datetime
 
@@ -14,26 +14,31 @@ class BaseModel:
     """ initializing a class """
     def __init__(self, *args, **kwargs) -> None:
         """creating class attributes """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.udated_at = datetime.now()
-
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, time)
-                elif key != "__class__":
-                    setattr(self, key, value)
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
         """ creating class method instances """
 
     def __str_(self) -> str:
-        """ converts data into string to be saved """
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        """
+        converts data into string to be saved
+        Return: class name, id and dictionary
+        """
+        return "[{}] ({}) {}".format
+    (self.__class__.__name__, self.id, self.__dict__)
 
     def save(self) -> None:
         """ method saves data and update time of changes """
@@ -43,7 +48,7 @@ class BaseModel:
     def to_dict(self) -> dict:
         """ saves converted string data into dictionary """
         temp_dico = self.__dict__.copy()
-        temp_dico["__class__"] = type(self).__name__
+        temp_dico["__class__"] = self.__class__.__name__
         temp_dico["created_at"] = temp_dico["created_at"].isoformat()
         temp_dico["updated_at"] = temp_dico["updated_at"].isoformat()
         return temp_dico
